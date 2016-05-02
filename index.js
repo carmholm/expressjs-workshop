99e58c8ec1968200e46da808f18e78384f89cc43
carmholm <ameliacarmenholmes@gmail.com>
2016-05-02 21:47:26 +0000

    Exercise 4 completed

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
 var express = require('express');
 var app = express();
+var fs = require('fs');
 
+var mysql = require('mysql');
+var connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'carmholm',
+  password: '',
+  database: 'reddit'
+});
 
-app.get('/', function (req, res) {
+app.get('/', function(req, res) {
   res.send('Hello World!');
 });
 
@@ -26,60 +34,116 @@ app.get('/', function (req, res) {
 //   res.send(result);
 // });
 
-function calculator (operation, num1, num2){
- var firstOp = parseInt(num1);
- var secondOp = parseInt(num2);
- if (operation === "add"){
-    return {
-      "operator": "add",
-      "firstOperand": firstOp,
-      "second Operand": secondOp,
-      "solution": firstOp + secondOp
-    };
-  }
-  else if (operation === "sub"){
-    return {
-      "operator": "sub",
-      "firstOperand": firstOp,
-      "second Operand": secondOp,
-      "solution": firstOp - secondOp
-    };
-  }
-  else if (operation === "mult"){
-    return {
-      "operator": "mult",
-      "firstOperand": firstOp,
-      "second Operand": secondOp,
-      "solution": firstOp * secondOp
-    };
-  }
-  else if (operation === "div"){
-    return {
-      "operator": "div",
-      "firstOperand": firstOp,
-      "second Operand": secondOp,
-      "solution": firstOp / secondOp
-    };
-  }
-    else {
-      return "Error";
-  }
+// function calculator (operation, num1, num2){
+// var firstOp = parseInt(num1);
+// var secondOp = parseInt(num2);
+// if (operation === "add"){
+//     return {
+//       "operator": "add",
+//       "firstOperand": firstOp,
+//       "second Operand": secondOp,
+//       "solution": firstOp + secondOp
+//     };
+//   }
+//   else if (operation === "sub"){
+//     return {
+//       "operator": "sub",
+//       "firstOperand": firstOp,
+//       "second Operand": secondOp,
+//       "solution": firstOp - secondOp
+//     };
+//   }
+//   else if (operation === "mult"){
+//     return {
+//       "operator": "mult",
+//       "firstOperand": firstOp,
+//       "second Operand": secondOp,
+//       "solution": firstOp * secondOp
+//     };
+//   }
+//   else if (operation === "div"){
+//     return {
+//       "operator": "div",
+//       "firstOperand": firstOp,
+//       "second Operand": secondOp,
+//       "solution": firstOp / secondOp
+//     };
+//   }
+//     else {
+//       return "Error";
+//   }
+// }
+
+// app.get('/calculator/:operation/:num1/:num2', function(req, res){
+//   console.log(res.status);
+//   var operation = req.params.operation;
+//   var num1 = req.params.num1;
+//   var num2 = req.params.num2;
+//   var result = calculator(operation, num1, num2);
+//   result == "Error" ? res.status(400).send("Bad Request") : res.send(result);
+// });
+
+function getRedditPosts(callback) {
+  connection.query(`SELECT p.title, p.url, p.createdAt, p.updatedAt, p.subredditId, u.username 
+      FROM posts p 
+      JOIN users u 
+      ON p.userId=u.Id 
+      WHERE userId=1
+      ORDER BY p.createdAt 
+      LIMIT 5`,
+    function(err, res) {
+      if (err) {
+        callback(err);
+      }
+      else {
+        callback(null, res);
+      }
+    });
 }
 
-app.get('/calculator/:operation/:num1/:num2', function(req, res){
-  console.log(res.status);
-  var operation = req.params.operation;
-  var num1 = req.params.num1;
-  var num2 = req.params.num2;
-  var result = calculator(operation, num1, num2);
-  result == "Error" ? res.status(400).send("Bad Request") : res.send(result);
+app.get('/posts', function(req, res) {
+  getRedditPosts(function(err, posts) {
+    if (err) {
+      res.status(500).send('oops try again later!');
+    }
+    else {
+
+      var allPosts = posts.map(function(post) {
+        return `
+          <li class="content-item">
+            <h2 class="content-item__title">
+              <a href=${post.url}>${post.title}</a>
+            </h2>
+            <p>Created by ${post.username}</p>
+          </li>`});
+        res.send(`
+          <div id="contents">
+            <h1>List of posts</h1>
+              <ul class="contents-list">
+                ${allPosts.join('')}
+              </ul>
+          </div>
+      `);
+    }
+  });
 });
+
+// app.get('/createContent', function(req, res){
+//   res.sendFile('form.html', function(err, fileContent) {
+//         if (err) {
+//             res.status(500).send('the file could not be read');
+//         }
+//         else {
+//             res.send(fileContent);
+//         }
+//     })
+// })
 
 
 /* YOU DON'T HAVE TO CHANGE ANYTHING BELOW THIS LINE :) */
 
 // Boilerplate code to start up the web server
-var server = app.listen(process.env.PORT, process.env.IP, function () {
+var server = app.listen(process.env.PORT, process.env.IP, function() {
   var host = server.address().address;
   var port = server.address().port;
 
