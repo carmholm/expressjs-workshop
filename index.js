7988f8d9411fd563c5abe7859e416819a2c1e554
carmholm <ameliacarmenholmes@gmail.com>
2016-05-02 18:03:04 +0000

    Exercise 1 completed

--- index.js
+++ index.js
@@ -5,9 +5,14 @@ app.get('/', function (req, res) {
   res.send('Hello World!');
 });
 
+//Exercise 1: Getting started!
+// Create a web server that can listen to requests for /hello, and respond with some HTML that says <h1>Hello World!</h1>
 
+app.get('/hello', function (req, res){
+  res.send('<h1>Hello World!</h1>');
+});
 
-
+//
 
 /* YOU DON'T HAVE TO CHANGE ANYTHING BELOW THIS LINE :) */
 
